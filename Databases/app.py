8e9db3222e0fda291c617c13a69746d12939c235
Zeynep Vızvız <serpilvzvz@gmail.com>
2025-01-05 02:47:16 +0300

    updated feedback, event attendance and swagger

--- Databases/app.py
+++ Databases/app.py
@@ -51,7 +51,7 @@ app.register_blueprint(event_bp, url_prefix='/events')
 app.register_blueprint(group_bp, url_prefix='/groups')
 app.register_blueprint(membership_bp, url_prefix='/membership')
 app.register_blueprint(tag_bp, url_prefix='/tags')
-app.register_blueprint(event_attendance_bp, url_prefix='/event_attendance')
+app.register_blueprint(event_attendance_bp)
 app.register_blueprint(feedback_bp, url_prefix='/feedback')
 
 # -----------------------------------------------------------------------------
